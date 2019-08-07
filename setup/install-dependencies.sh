7c4aad99eb1033328eebf8235629f0efd8c5ce8a
JavierCabello-GDB <javier@geodb.com>
2019-08-07 10:43:08 +0200

    Bug Fixed && Tested in a Ubuntu environment raised on Demand

--- setup/install-dependencies.sh
+++ setup/install-dependencies.sh
@@ -1,64 +1,43 @@
-# !/bin/bash
+#!/bin/bash
 
 check_returnCode() {
         if [ $1 -eq 0 ]; then
-                echo -e "INFO:.... El proceso se ha ejecutado con éxito"
+                echo -e "INFO:.... Proccess Succeed"
         else
-                >&2 echo -e "ERROR:.... El proceso se ha ejecutado con error: $1"
-                echo -e "INFO:Saliendo..."
+                >&2 echo -e "ERROR.... Proccess ERROR: $1"
+                echo -e "INFO:Please check errors and retry..."
                 exit $1
         fi
 }
 
-checkCURL(){
-  command -v curl >/dev/null 2>&1 || { installCURL; }
+installDependencies() {
+  apt-get update
+  programs=(curl make docker docker-compose jq)
+
+  for program in "${programs[@]}"; do
+      if ! command -v "$program" > /dev/null 2>&1; then
+
+          echo
+          echo "========================================================="
+          echo "Now installing $program"
+          echo "========================================================="
+          echo
+          apt-get install "$program" -y
+          sleep 1s
+      fi
+  done
 }
 
-installCURL(){
+installMakeGuile() {
   echo
   echo "========================================================="
-  echo "Now installing CURL"
+  echo "Now installing make-guile"
   echo "========================================================="
   echo
+
+  apt-get install make-guile -y
 
   sleep 1s
-
-  apt-get install curl -y
-}
-
-checkDocker(){
-  command -v docker >/dev/null 2>&1 || { installDocker; }
-}
-
-installDocker(){
-  echo
-  echo "========================================================="
-  echo "Now installing docker"
-  echo "========================================================="
-  echo
-
-  sleep 1s
-
-  apt-get install docker.io -y
-
-  addUser=`logname`
-  usermod -a -G docker $addUser
-}
-
-checkDockerCompose(){
-  command -v docker-compose >/dev/null 2>&1 || { installDockerCompose; }
-}
-
-installDockerCompose(){
-  echo
-  echo "========================================================="
-  echo "Now installing docker-compose"
-  echo "========================================================="
-  echo
-
-  sleep 1s
-
-  apt-get install docker-compose -y
 }
 
 installLibtool(){
@@ -74,20 +53,38 @@ installLibtool(){
   apt-get install libtool libltdl-dev -y
 }
 
-checkCURL(){
-  command -v jq >/dev/null 2>&1 || { installJQ; }
-}
+checkEnvironment(){
 
-installJQ(){
   echo
   echo "========================================================="
-  echo "Now installing JQ"
+  echo "Checking and setting ENV"
   echo "========================================================="
   echo
 
   sleep 1s
 
-  apt-get install jq -y
+  while IFS="" read -r checkVar || [ -n "$p" ]
+  do
+    if grep -Fxq "$checkVar" /etc/bash.bashrc
+    then
+        echo "$checkVar IS SET"
+    else
+        echo $checkVar >> /etc/bash.bashrc
+    fi
+  done < fabric-environment.sh
+
+  # cp ./fabric-environment.sh /etc/profile.d/fabric-environment.sh
+}
+
+addUserToDockerGroup() {
+
+  echo
+  echo "========================================================="
+  echo "Adding user to docker group"
+  echo "========================================================="
+  echo
+
+  usermod -a -G docker $(logname)
 }
 
 if [ `id -u` != "0" ]; then
@@ -95,19 +92,19 @@ if [ `id -u` != "0" ]; then
   exit 1
 fi
 
-checkCURL
+installDependencies
 check_returnCode $?
 
-checkDocker
-check_returnCode $?
-
-checkDockerCompose
-check_returnCode $?
-
-checkJQ
+installMakeGuile
 check_returnCode $?
 
 installLibtool
+check_returnCode $?
+
+checkEnvironment
+check_returnCode $?
+
+addUserToDockerGroup
 check_returnCode $?
 
 echo
